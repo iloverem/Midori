d9ed15d6b0348efc4b1846ff38d15ea4e7cfea58
iloverem <iloverem@users.noreply.github.com>
2017-01-27 13:17:56 +1300

    Added a dance command /o/

---
+++ src/Rem/Modules/Fun/RemDanceCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
+
+namespace MidoriBot.Modules.Fun
+{
+    [Name("Fun")]
+    public class RemDanceCommand : ModuleBase
+    {
+        [Command("Dance"), Summary("Dances!")]
+        public async Task DanceCommand()
+        {
+            bool Left = true;
+            int Loops = 0;
+            IUserMessage Message = await Context.Channel.SendMessageAsync(@"\o\");
+            while (Loops < 10)
+            {
+                Left = !Left;
+                if (Left)
+                {
+                    await Message.ModifyAsync(x =>
+                    {
+                        x.Content = @"\o\";
+                    });
+                }
+                else
+                {
+                    await Message.ModifyAsync(x =>
+                    {
+                        x.Content = @"/o/";
+                    });
+                }
+                Loops++;
+            }
+        }
+    }
+}
